8ff04eba72c73503f1c87ae617edb653c9a8c229
bs <bs>
2018-05-23 11:02:52 +0800

    发布课程通知Fragment以及逻辑 点击按钮事件触发闪退，已经注释掉 课程通知类加入了CourseID,目前准备搞随机数，求更优方法
    Signed-off-by: bs <bs>

--- app/src/main/java/com/example/dell/afinal/bean/CourseNotification.java
+++ app/src/main/java/com/example/dell/afinal/bean/CourseNotification.java
@@ -1,6 +1,7 @@
 package com.example.dell.afinal.bean;
 
 import cn.bmob.v3.BmobObject;
+import cn.bmob.v3.datatype.BmobRelation;
 
 // 系统消息类
 public class CourseNotification extends BmobObject {
@@ -8,6 +9,29 @@ public class CourseNotification extends BmobObject {
     private String title;     // 通知的标题
     private String content;   // 通知的内容
     private String date;      // 通知创建的日期
+    private String CourseId; // 课程ID,用户通知分区
+    private User author;     //课程通知，即老师
+    private  BmobRelation student; //待接收通知的学生
+
+    public BmobRelation getStudent() {
+        return student;
+    }
+
+    public void setStudent(BmobRelation student) {
+        this.student = student;
+    }
+
+    public  void  setCourseId(String ID){this.CourseId=ID;}
+
+    public String getCourseId(){
+        return this.CourseId;
+    }
+    public void setAuthor(User user){
+        this.author = user;
+    }
+    public User getAuthor(){
+        return this.author;
+    }
 
     public String getTitle() {
         return title;
